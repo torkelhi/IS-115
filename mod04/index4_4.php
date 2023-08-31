bdcede8f47da6aa8d0f51156d8e78e54dc4be741
torkelhi <torkelhi@uia.no>
2023-08-31 14:19:09 +0200

    multidimensjonell array

--- mod04/index4_4.php
+++ mod04/index4_4.php
@@ -1,7 +1,7 @@
 <h4> <a href="./index.php">Tilbake til Oppgave Oversikt</a> </h4>
 <?php
 
-class Jobbannonse{
+/*class Jobbannonse{
     public $overskrift;
     public $beskrivelse;
     public $frist;
@@ -23,15 +23,24 @@ class Jobbannonse{
         echo "</table>";
         
       }
-}
-$it= new Jobbannonse("IT-konsulent hos knowit","Intern-prosjekter i firma","2023-10-05","Bergen");
-$advokat= new Jobbannonse("Advokat","Advokat fullmektig","2024-01-01","Oslo");
-$politi= new Jobbannonse("Pratrulje Leder","Ansvarlig for forvaltning av arbeidskraft til oppdrag","2023-12-01","Finnmark");
+}*/
+
+
+$it= array("IT-konsulent hos knowit","Intern-prosjekter i firma","2023-10-05","Bergen");
+$advokat= array("Advokat","Advokat fullmektig","2024-01-01","Oslo");
+$politi= array("Pratrulje Leder","Ansvarlig for forvaltning av arbeidskraft til oppdrag","2023-12-01","Finnmark");
 
 $jobbannonser = array($it,$advokat,$politi);
 
 foreach($jobbannonser as $jobbannonse) {
-    $jobbannonse->show();
-    echo "<br>";
+ echo "<table style='border-style: solid; border-color: black; padding: 20px; width: 500px;'>";
+  foreach($jobbannonse as $val){
+    if($val === $jobbannonse[0]){
+      echo "<tr> <th> {$val}  </th> </tr >";
+    }else{
+    echo "<tr> <td> {$val}  </td> </tr >";
+    }
+  }
+  echo "</table>";
 }
 ?>
